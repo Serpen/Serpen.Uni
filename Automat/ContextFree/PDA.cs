58596c9fcd73cd7e5d05df2e4212007c7c031ffc
Marco <serpen@hotmail.de>
2019-09-24 18:51:23 +0200

    tidy PDA

--- Automat/ContextFree/PDA.cs
+++ Automat/ContextFree/PDA.cs
@@ -2,18 +2,19 @@ using System.Linq;
 using System.Collections.Generic;
 
 namespace Serpen.Uni.Automat.ContextFree {
-    
+
     /// <summary>
     /// nondeterministic PDA with Stack Symbol, which must end as empty stack and accepted state
     /// </summary>
     public abstract class PDA : AutomatBase<PDATransformKey, PDATransformValue[]> {
 
-        protected static readonly char[] EXTRASYMBOLS =  new char[] {'§','$','%','&'};
+        protected int MAX_RUNS_OR_STACK = 10000;
+        protected static readonly char[] EXTRASYMBOLS = new char[] { '§', '$', '%', '&' };
         public const char START = '$';
         public readonly char[] WorkAlphabet;
         //public readonly PDATransform Transform;
         public readonly char StartStackSymbol;
-        
+
         /// <summary>
         /// Create a PDA which accepts when ending in Accepted States
         /// </summary>
@@ -38,10 +39,8 @@ namespace Serpen.Uni.Automat.ContextFree {
 
 
         protected override void CheckConstraints() {
-            foreach (var t in Transform)
-            {
-                for (int i = 0; i < t.Value.Length; i++)
-                {
+            foreach (var t in Transform) {
+                for (int i = 0; i < t.Value.Length; i++) {
                     if (t.Key.q > StatesCount | t.Value[i].qNext > StatesCount)
                         throw new StateException(t.Key.q);
                     else if (t.Key.ci.HasValue && !Alphabet.Contains(t.Key.ci.Value))
@@ -50,71 +49,69 @@ namespace Serpen.Uni.Automat.ContextFree {
                         throw new AlphabetException(t.Key.cw.Value);
                     else if (t.Value[i].cw2 != null && t.Value[i].cw2 != "" && !WorkAlphabet.Contains(t.Value[i].cw2[0]))
                         throw new AlphabetException(t.Value[i].cw2[0]);
-                    
+
                 }
             }
             for (int i = 0; i < AcceptedStates.Length; i++)
                 if (AcceptedStates[i] > StatesCount)
-                        throw new StateException(AcceptedStates[i]);
-            
+                    throw new StateException(AcceptedStates[i]);
+
         }
 
         //nondeterministic makes it grow exponetially, maybe it is best, to do a BFS instead of DFS, follow one path to its possible end
         public PDAConfig[] GoChar(PDAConfig[] pcfgs) {
             var retCfgs = new List<PDAConfig>();
 
-            if (pcfgs.Length > 100000) {
+            if (pcfgs.Length > MAX_RUNS_OR_STACK) {
                 Utils.DebugMessage($"Stack >= {pcfgs.Length}, abort", this);
                 return new PDAConfig[0];
             }
 
-            foreach (var pcfg in pcfgs)
-            {
+            foreach (var pcfg in pcfgs) {
                 PDATransformKey[] qStart = new PDATransformKey[1];
 
                 //if word is empty, maybe only e-Transform is needed
                 if (pcfg.word != "") {
                     if (pcfg.Stack.Any())
                         qStart[0] = new PDATransformKey(pcfg.q, pcfg.word[0], pcfg.Stack[0]);
-                        // qStart[0] = new PDATransformKey(pcfg.q, pcfg.word[0], pcfg.Stack[pcfg.Stack.Length-1]);
+                    // qStart[0] = new PDATransformKey(pcfg.q, pcfg.word[0], pcfg.Stack[pcfg.Stack.Length-1]);
                     else
                         qStart[0] = new PDATransformKey(pcfg.q, pcfg.word[0], null);
-                        // continue; //TODO: why that //maybe empty stack that is not accepted should be thrown away
+                    // continue; //TODO: why that //maybe empty stack that is not accepted should be thrown away
 
                 } else {
                     if (pcfg.Stack.Any())
                         qStart[0] = new PDATransformKey(pcfg.q, null, pcfg.Stack[0]);
                     else
                         qStart[0] = new PDATransformKey(pcfg.q, null, null);
-                        // continue; //TODO: why that
+                    // continue; //TODO: why that
                 }
 
                 PDATransformValue[] qNext;
-                
+
                 //get all possible (e-)transforms
                 if (((PDATransform)Transform).TryGetValue(ref qStart, out qNext)) {
                     //iterate each cuple of start and next transform
-                    for (int j = 0; j < qNext.Length; j++)
-                    {
+                    for (int j = 0; j < qNext.Length; j++) {
                         var newStack = new Stack<char>(pcfg.Stack.Reverse());
                         var qNextj = qNext[j];
 
                         //stack symbol cw2 replaces cw
                         if (qStart[j].cw.HasValue && qNext[j].cw2 != null) {
                             newStack.Pop();
-                            for (int i = qNextj.cw2.Length-1; i >= 0 ; i--)
+                            for (int i = qNextj.cw2.Length - 1; i >= 0; i--)
                                 newStack.Push(qNextj.cw2[i]);
 
-                        //cw was used, replace with e, so pop
+                            //cw was used, replace with e, so pop
                         } else if (qStart[j].cw.HasValue && qNext[j].cw2 == null) {
                             newStack.Pop();
-                        
-                        //any (e-cw) should be replaced by cw2
+
+                            //any (e-cw) should be replaced by cw2
                         } else if (!qStart[j].cw.HasValue && qNext[j].cw2 != null) {
-                            for (int i = qNextj.cw2.Length-1; i >= 0 ; i--)
+                            for (int i = qNextj.cw2.Length - 1; i >= 0; i--)
                                 newStack.Push(qNextj.cw2[i]);
 
-                        //cw's not relevant, no stack action
+                            //cw's not relevant, no stack action
                         } else if (!qStart[j].cw.HasValue && qNext[j].cw2 == null) {
                             //do nothing
                         } else
@@ -127,7 +124,7 @@ namespace Serpen.Uni.Automat.ContextFree {
                                 newWord = pcfg.word.Substring(1);
 
                         retCfgs.Add(new PDAConfig(qNext[j].qNext, newWord, newStack.ToArray(), pcfg));
-                        
+
                     } //next j
 
                 } //end if tryGetValue
@@ -139,7 +136,7 @@ namespace Serpen.Uni.Automat.ContextFree {
                 retCfgs2 = new List<PDAConfig>();
                 int maxStackLen = int.MaxValue;
                 foreach (var rcfg in retCfgs) {
-                    if (rcfg.Stack.Length <= rcfg.word.Length*3) { 
+                    if (rcfg.Stack.Length <= rcfg.word.Length * 3) {
                         maxStackLen = rcfg.word.Length;
                         retCfgs2.Add(rcfg);
                     }
@@ -150,39 +147,7 @@ namespace Serpen.Uni.Automat.ContextFree {
             return retCfgs2.Distinct().ToArray();
         }
 
-        public override bool AcceptWord(string w) {
-            return AcceptWordBySomeCrappyMiscOfBoth(w);
-        }
-
-        internal bool AcceptWordBySomeCrappyMiscOfBoth(string w) {
-            //construct start config
-            PDAConfig[] pcfg;
-            if (StartStackSymbol != 0)
-                pcfg = new PDAConfig[] {new PDAConfig(StartState, w, new char[] {StartStackSymbol}, null)};
-            else
-                pcfg = new PDAConfig[] {new PDAConfig(StartState, w, new char[] {}, null)};
-
-            //while any pcfg exists
-            while (pcfg.Length > 0) { //&& (pcfg.Where((a) => a.Stack.Length>0).Any())
-                pcfg = GoChar(pcfg);
-
-                var notfoolstates = new List<PDAConfig>();
-                //check if a cfg has word and stack cleared and ends in accepted states
-                foreach (var p in pcfg) {
-                    if (p.Stack.Count() == 0 && p.word.Length == 0) {
-                        if (IsAcceptedState(p.q)) {
-                            return true;
-                        } else {
-                            System.Console.WriteLine($"{pcfg} seems useless");
-                        } //state is useless and could't move anymore??? --> IS THAT TRUE???
-                    } else
-                        notfoolstates.Add(p);
-                }
-                pcfg = notfoolstates.ToArray();
-            }
-
-            return false;
-        }
+        public abstract override bool AcceptWord(string w);
 
         public override System.Tuple<int, int, string>[] VisualizationLines() {
             var tcol = new System.Collections.Generic.List<System.Tuple<int, int, string>>();
--- Automat/ContextFree/StackPDA.cs
+++ Automat/ContextFree/StackPDA.cs
@@ -3,8 +3,6 @@ using System.Linq;
 namespace Serpen.Uni.Automat.ContextFree {
     public class StackPDA : PDA {
 
-        public int MAX_RUNS_OR_STACK = 10000;
-
         /// <summary>
         /// Create PDA which accepts by empty stack
         /// </summary>
@@ -14,16 +12,15 @@ namespace Serpen.Uni.Automat.ContextFree {
         /// <param name="Transform"></param>
         /// <param name="StartState">State from which to start</param>
         /// <param name="Startsymbol">Inital Stack Population</param>
-        public StackPDA(string name, uint StatesCount, char[] InputAlphabet, char[] Workalphabet, PDATransform Transform, uint StartState, char Startsymbol) 
-        : base(name, StatesCount, InputAlphabet, Workalphabet, Transform, StartState, Startsymbol, new uint[] {}) {
-            
+        public StackPDA(string name, uint StatesCount, char[] InputAlphabet, char[] Workalphabet, PDATransform Transform, uint StartState, char Startsymbol)
+        : base(name, StatesCount, InputAlphabet, Workalphabet, Transform, StartState, Startsymbol, new uint[] { }) {
+
             //Make all States acceptable for 
             base.AcceptedStates = new uint[StatesCount];
             for (uint i = 0; i < StatesCount; i++)
                 base.AcceptedStates[i] = i;
 
         }
-       
 
         [System.ComponentModel.Description("1659_L33")]
         public static explicit operator StackPDA(StatePDA pda) {
@@ -32,52 +29,48 @@ namespace Serpen.Uni.Automat.ContextFree {
             char extra_symbol = Utils.NextFreeCapitalLetter(pda.Alphabet.Concat(pda.WorkAlphabet).ToArray(), EXTRASYMBOLS[0], EXTRASYMBOLS);
             var spdaWorkAlphabet = new System.Collections.Generic.List<char>(pda.WorkAlphabet);
             spdaWorkAlphabet.Add(extra_symbol);
-            
+
             newt.Add(0, null, null, pda.StartStackSymbol.ToString(), 1);
 
             uint qPump = pda.StatesCount;
 
-            for (int i = 0; i < pda.Transform.Count; i++)
-            {
+            for (int i = 0; i < pda.Transform.Count; i++) {
                 var t = pda.Transform.ElementAt(i);
-                for (int j = 0; j < t.Value.Length; j++)
-                {
+                for (int j = 0; j < t.Value.Length; j++) {
                     //inc all States cause of new first § state
-                    newt.AddM(t.Key.q+1, t.Key.ci, t.Key.cw, t.Value[j].cw2, t.Value[j].qNext+1);
+                    newt.AddM(t.Key.q + 1, t.Key.ci, t.Key.cw, t.Value[j].cw2, t.Value[j].qNext + 1);
 
                     //accepted state goes to qPump
                     if (pda.IsAcceptedState(t.Key.q)) {
-                        newt.Add(t.Key.q+1, null, null, null, qPump);
+                        newt.Add(t.Key.q + 1, null, null, null, qPump);
                     }
                 }
             }
 
-            foreach (char c in pda.Alphabet)
-            {
+            foreach (char c in pda.Alphabet) {
                 if (!spdaWorkAlphabet.Contains(c))
                     spdaWorkAlphabet.Add(c);
-                newt.Add(qPump,null, c, null, qPump);
+                newt.Add(qPump, null, c, null, qPump);
             }
             newt.Add(qPump, null, extra_symbol, null, qPump);
 
-            return new StackPDA($"SPDA_({pda.Name})", pda.StatesCount+1, pda.Alphabet, spdaWorkAlphabet.ToArray(), newt, 0, extra_symbol);
+            return new StackPDA($"SPDA_({pda.Name})", pda.StatesCount + 1, pda.Alphabet, spdaWorkAlphabet.ToArray(), newt, 0, extra_symbol);
         }
-
 
         public override bool AcceptWord(string w) {
             int runCount = 0;
             //construct start config
             PDAConfig[] pcfgs;
             if (StartStackSymbol != 0)
-                pcfgs = new PDAConfig[] {new PDAConfig(StartState, w, new char[] {StartStackSymbol}, null)};
+                pcfgs = new PDAConfig[] { new PDAConfig(StartState, w, new char[] { StartStackSymbol }, null) };
             else
-                pcfgs = new PDAConfig[] {new PDAConfig(StartState, w, new char[] {}, null)};
+                pcfgs = new PDAConfig[] { new PDAConfig(StartState, w, new char[] { }, null) };
 
 
             //while any pcfg exists
             while (pcfgs.Length > 0) { //&& (pcfg.Where((a) => a.Stack.Length>0).Any())
                 foreach (var p in pcfgs) {
-                    if ((p.Stack.Count() == 0 || (p.Stack[p.Stack.Count()-1] == this.StartStackSymbol && p.Stack.Count() == 1)) && p.word.Length == 0) {
+                    if ((p.Stack.Count() == 0 || (p.Stack[p.Stack.Count() - 1] == this.StartStackSymbol && p.Stack.Count() == 1)) && p.word.Length == 0) {
                         return true;
                     }
                 }
@@ -90,7 +83,7 @@ namespace Serpen.Uni.Automat.ContextFree {
                     return false;
                 }
                 //check if a cfg has word and stack cleared and ends in accepted states
-                
+
             }
 
             return false;
@@ -99,39 +92,36 @@ namespace Serpen.Uni.Automat.ContextFree {
         [System.ComponentModel.Description("1659_L3.1_P76")]
         public static explicit operator StackPDA(ContextFree.CFGrammer cfg) {
             var t = new ContextFree.PDATransform();
-            var sn = new System.Collections.Generic.Dictionary<uint,string>();
-            
+            var sn = new System.Collections.Generic.Dictionary<uint, string>();
+
             const uint qSim = 1;
-            uint q = qSim+1;
+            uint q = qSim + 1;
 
             // t.Add(0,null, null, ContextFree.PDA.START.ToString(), 1);
             // sn.Add(0, "0");
             t.Add(0, null, null, cfg.StartSymbol.ToString(), qSim);
             sn.Add(0, "start");
 
-            foreach (char c in cfg.Terminals)
-            {
+            foreach (char c in cfg.Terminals) {
                 t.Add(qSim, c, c, null, qSim);
             }
             t.Add(qSim, null, ContextFree.PDA.START, null, qSim);
             sn.Add(qSim, "sim");
 
-            foreach (var r in cfg.Rules)
-            {
+            foreach (var r in cfg.Rules) {
                 foreach (string body in r.Value) {
-                    if (body.Length>2) {
-                        t.AddM(qSim, null, r.Key, body.Substring(body.Length-1,1), q);
-                        sn.TryAdd(q, $"{q}; {r.Key}=>{body}");  
-                        for (int i=body.Length-2; i > 0 ; i--)
-                        {
-                            t.AddM(q, null, null, body.Substring(i,1), ++q);
+                    if (body.Length > 2) {
+                        t.AddM(qSim, null, r.Key, body.Substring(body.Length - 1, 1), q);
+                        sn.TryAdd(q, $"{q}; {r.Key}=>{body}");
+                        for (int i = body.Length - 2; i > 0; i--) {
+                            t.AddM(q, null, null, body.Substring(i, 1), ++q);
                             sn.TryAdd(q, $"{q}; {r.Key}=>{body}");
                         }
-                        t.AddM(q, null, null, body.Substring(0,1), qSim);
+                        t.AddM(q, null, null, body.Substring(0, 1), qSim);
                         q++;
                     } else if (body.Length == 2) {
-                        t.AddM(qSim, null, r.Key, body.Substring(1,1), q);
-                        t.AddM(q, null, null, body.Substring(0,1), qSim);
+                        t.AddM(qSim, null, r.Key, body.Substring(1, 1), q);
+                        t.AddM(q, null, null, body.Substring(0, 1), qSim);
                         sn.TryAdd(q, $"{q}; {r.Key}=>{body}");
                         q++;
 
@@ -141,16 +131,13 @@ namespace Serpen.Uni.Automat.ContextFree {
                 }
             }
 
-
-
             var WorkAlphabet = new System.Collections.Generic.List<char>();
             WorkAlphabet.AddRange(cfg.Terminals);
             WorkAlphabet.AddRange(cfg.Variables);
             // WorkAlphabet.Add(ContextFree.PDA.START);
 
             var spda = new ContextFree.StackPDA($"SPDA_({cfg.Name})", q, cfg.Terminals, WorkAlphabet.ToArray(), t, 0, ContextFree.PDA.START);
-            for (uint i = 0; i < spda.StatesCount; i++)
-            {
+            for (uint i = 0; i < spda.StatesCount; i++) {
                 spda.States[i] = sn[i];
             }
             return spda;
