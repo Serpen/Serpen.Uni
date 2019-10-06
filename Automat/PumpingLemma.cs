a47820450fd15906acb482e7e7efdee57a43b161
Marco <serpen@hotmail.de>
2019-10-06 08:56:15 +0200

    pumpinglemma

--- Automat/PumpingLemma.cs
+++ Automat/PumpingLemma.cs
@@ -1,57 +1,89 @@
+using System.Linq;
+
 namespace Serpen.Uni.Automat {
-    public class PumpingLemma {
+    public static class PumpingLemma {
+
+        public enum PumpResult
+        {
+            Unknown, Pumpable, NotPumpable, NoAcceptedWordFound, NoAcceptedWordExists
+        }
+        
+        //should return certificate instead of true/false, cover more possible outcomes 
+        /// <returns>bool empiric _guess_ if its pumpbar</returns>
+        public static PumpResult TestPumpbar(IAutomat Automat, int pumpLaenge, int words = 200, int maxWordLen = 50, int exponentMax = 20, int exponentCount = 10) {
+            int allAcceptedWordsPass = 0;
+
+            if (Automat.AcceptedStates.Length == 0)
+                return PumpResult.NoAcceptedWordExists;
+
+            bool foundAtLeastOneAcceptedWordForTest = false;
+            foreach (string w in Automat.GetRandomWords(words, pumpLaenge, maxWordLen)) {
+                if (Automat.AcceptWord(w)) {
+                    foundAtLeastOneAcceptedWordForTest = true;
+                    bool foundAccepted = false;
+                    allAcceptedWordsPass = 0;
+                    foreach (string[] strParts in StringParts3(w)) {
+
+                        if (!(w.Length >= pumpLaenge)) {
+                            Utils.DebugMessage($"word |w={w}|>={pumpLaenge}", Automat, Utils.eDebugLogLevel.Verbose);
+                            continue; // throw new PumpingException($"|w|={w.Length}<{pumpLaenge}");
+                        }
+
+                        string x = strParts[0];
+                        string y = strParts[1];
+                        string z = strParts[2];
+
+                        Utils.DebugMessage($"word {w}=({x}.{y}.{z})", Automat, Utils.eDebugLogLevel.Verbose);
+                        if (!((x + y).Length <= pumpLaenge)) {
+                            Utils.DebugMessage($"word |x+y={x.Length}+{y.Length}|<={pumpLaenge}", Automat, Utils.eDebugLogLevel.Verbose);
+                            continue; // throw new PumpingException($"|xy|={(x + y).Length}>{pumpLaenge}");
+                        }
+                        if (y.Length == 0) {
+                            Utils.DebugMessage($"word |y={y}|=0", Automat, Utils.eDebugLogLevel.Verbose);
+                            continue; // throw new PumpingException($"|y|=0, y={Utils.EPSILON}");
+                        }
 
 
-        public static bool TestPumpbar(IAutomat Automat, int pumpLaenge) {
-            // string w = Automat.GetRandomWord();
-            // pumpLaenge = w.Length;
-            for (int l = 1; l < 10; l++) {
-                foreach (string w in Automat.GetRandomWords(10000, 10 * l)) {
-                    for (int i = 0; i < w.Length; i++) {
-                        for (int j = 0; j < w.Length - i; j++) {
-                            string x = w.Substring(0, i);
-                            string y = w.Substring(i, j);
-                            string z = w.Substring(i + j, w.Length - i - j);
-                            // System.Console.WriteLine($"{x} {y} {z}".Trim());
-
-                            Utils.DebugMessage($"word {w}=({x}.{y}.{z})", Automat, Utils.eDebugLogLevel.Verbose);
-
-                            if (!(w.Length >= pumpLaenge)) {
-                                Utils.DebugMessage($"word |w={w}|>={pumpLaenge}", Automat, Utils.eDebugLogLevel.Verbose);
-                                continue; // throw new PumpingException($"|w|={w.Length}<{pumpLaenge}");
-                            }
-                            if (!((x + y).Length <= pumpLaenge)) {
-                                Utils.DebugMessage($"word |x+y={x.Length}+{y.Length}|<={pumpLaenge}", Automat, Utils.eDebugLogLevel.Verbose);
-                                continue; // throw new PumpingException($"|xy|={(x + y).Length}>{pumpLaenge}");
-                            }
-                            if (y.Length == 0) {
-                                Utils.DebugMessage($"word |y={y}|=0", Automat, Utils.eDebugLogLevel.Verbose);
-                                continue; // throw new PumpingException($"|y|=0, y={Utils.EPSILON}");
-                            }
-
-                            bool first = Automat.AcceptWord(w);
-                            if (first) {
-                                bool second = false;
-
-                                for (int k = 0; k < 10; k++) {
-                                    int r = Utils.RND.Next(1, 100);
-                                    string wi = x + string.Concat(System.Linq.Enumerable.Repeat(y, r)) + z;
-                                    second = Automat.AcceptWord(wi);
-                                    Utils.DebugMessage($"word {wi}: {second}", Automat, Utils.eDebugLogLevel.Verbose);
-                                    if (!second)
-                                        break;
-                                }
-
-                                if (first && second) {
-                                    // Utils.DebugMessage($"{w} {pumpLaenge}-pumpbar for {x}.{y}.{z}", Automat, Utils.eDebugLogLevel.Normal);
-                                    return true;
-                                }
+                        for (int k = 0; k < exponentCount; k++) {
+                            int r = Utils.RND.Next(1, exponentMax);
+                            string wi = x + string.Concat(System.Linq.Enumerable.Repeat(y, r)) + z;
+                            foundAccepted = Automat.AcceptWord(wi);
+                            Utils.DebugMessage($"word {wi}: {foundAccepted}", Automat, Utils.eDebugLogLevel.Verbose);
+                            if (foundAccepted) {
+                                Utils.DebugMessage($"{w} {pumpLaenge}-pumpbar for {x}.{y}.{z}", Automat, Utils.eDebugLogLevel.Verbose);
+                                allAcceptedWordsPass++;
                             }
                         }
-                    }
+                        if (allAcceptedWordsPass >= exponentCount)
+                            break;
+                    } //next strParts
+                    if (allAcceptedWordsPass < exponentCount)
+                        return PumpResult.NotPumpable;
+                } //endif AcceptWord
+            } //next w
+            if (!foundAtLeastOneAcceptedWordForTest)
+                return PumpResult.NoAcceptedWordFound;
+
+            if (allAcceptedWordsPass > 0) {
+                Utils.DebugMessage($"{pumpLaenge}-pumpbar for |{allAcceptedWordsPass}| words", Automat, Utils.eDebugLogLevel.Verbose);
+                return PumpResult.Pumpable;
+            } else
+                Utils.DebugMessage($"not {pumpLaenge}-pumpbar", Automat, Utils.eDebugLogLevel.Verbose);
+            return PumpResult.NotPumpable;
+        }
+
+        static string[][] StringParts3(string w) {
+            const int PARTS = 3;
+            var words = new System.Collections.Generic.List<string[]>();
+            for (int i = 0; i < w.Length; i++)
+                for (int j = 0; j < w.Length - i; j++) {
+                    string[] strParts = new string[PARTS];
+                    strParts[0] = w.Substring(0, i);
+                    strParts[1] = w.Substring(i, j);
+                    strParts[2] = w.Substring(i + j, w.Length - i - j);
+                    words.Add(strParts);
                 }
-            }
-            return false;
+            return words.Distinct().ToArray();
         }
     }
 
