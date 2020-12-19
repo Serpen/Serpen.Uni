edf055cf88032d6a2a4c3981d9e02a20effefaca
Serpen <serpen@hotmail.de>
2020-12-20 00:22:46 +0100

    heapsort

--- SortAlg.cs
+++ SortAlg.cs
@@ -31,12 +31,12 @@ namespace Serpen.Uni {
 
             for (int i = 0; i < nextList.Length; i++) {
                 int lowestIndex = i;
-                for (int j = i+1; j < nextList.Length; j++) {
-                    if (nextList[j].CompareTo(nextList[lowestIndex])<0)
+                for (int j = i + 1; j < nextList.Length; j++) {
+                    if (nextList[j].CompareTo(nextList[lowestIndex]) < 0)
                         lowestIndex = j;
                 }
                 (nextList[lowestIndex], nextList[i]) = (nextList[i], nextList[lowestIndex]);
-                
+
             }
             return nextList;
         }
@@ -45,18 +45,57 @@ namespace Serpen.Uni {
         public static T[] BubbleSort<T>(T[] array) where T : struct, System.IComparable {
             T[] nextList = (T[])array.Clone();
 
-            for (int i = 0; i < nextList.Length; i++)
-            {
-                for (int j = i+1; j < nextList.Length; j++)
-                {
+            for (int i = 0; i < nextList.Length; i++) {
+                for (int j = i + 1; j < nextList.Length; j++) {
                     if (nextList[j].CompareTo(nextList[i]) < 0)
                         (nextList[i], nextList[j]) = (nextList[j], nextList[i]);
                 }
             }
             return nextList;
         }
-        public static T[] MergeSort<T>(T[] array) => throw new System.NotImplementedException();
-        public static T[] QuickSort<T>(T[] array) => throw new System.NotImplementedException();
-        public static T[] HeapSort<T>(T[] array) => throw new System.NotImplementedException();
+        public static T[] MergeSort<T>(T[] array) where T : System.IComparable  => throw new System.NotImplementedException();
+        public static T[] QuickSort<T>(T[] array) where T : System.IComparable  => throw new System.NotImplementedException();
+
+        [AlgorithmComplexity("n*log(n)")]
+        public static T[] HeapSort<T>(in T[] array) where T : System.IComparable {
+            int n = array.Length;
+            T[] array2 = (T[])array.Clone();
+
+            for (int i = n / 2; i >= 1; i--)
+                Reheap<T>(ref array2, i, n);
+
+            for (int i = n; i >= 2; i--) {
+                (array2[0], array2[i - 1]) = (array2[i - 1], array2[0]);
+                Reheap<T>(ref array2, 1, i - 1);
+            }
+            return array2;
+        }
+
+        static void Reheap<T>(ref T[] array, int i, int k) where T : System.IComparable {
+            int j = i;
+            int son = 0;
+            bool endLoop = false;
+
+            do {
+                if (2 * j > k)
+                    break;
+                else {
+                    if (2 * j + 1 <= k)
+                        if (array[2 * j - 1].CompareTo(array[2 * j]) < 0)
+                            son = 2 * j;
+                        else
+                            son = 2 * j + 1;
+                    else
+                        son = 2 * j;
+
+                    if (array[son - 1].CompareTo(array[j - 1]) < 0) {
+                        (array[j - 1], array[son - 1]) = (array[son - 1], array[j - 1]);
+                        j = son;
+                    } else
+                        endLoop = true;
+                }
+            } while (!endLoop);
+            Utils.DebugMessage(System.String.Join(',', array), Utils.eDebugLogLevel.Verbose);
+        }
     }
 }
